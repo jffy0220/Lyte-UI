1f64bfd8fa8ca8564bc8b970b9fec70cf4af1da1
Justin Farley <justin.farley94@gmail.com>
2022-09-12 16:38:15 -0400

    UI INIT

---
+++ lyte-ui/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <v-app>
+    <v-app-bar color="deep-purple" dark elevation="3" app>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+      <v-toolbar-title>Lyte</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-cart</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Orders</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-storefront</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Inventory</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-office-building</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Warehouse</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-main>
+      Hello World!
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data: () => ({
+    drawer: false,
+    group: null
+  })
+};
+</script>
